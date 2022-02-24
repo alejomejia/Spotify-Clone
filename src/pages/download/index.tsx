4a089cb0ad882b2df501a13ca10b01d6b8571a4c
Alejandro Mejia <alejomejiacuartas@outlook.com>
2022-02-24 12:29:25 -0500

    add Download page

---
+++ src/pages/download/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'components/Head/Head'
+
+import Layout from 'components/Layout/Layout'
+
+const DownloadPage: NextPage = () => {
+  return (
+    <>
+      <Head title="Download | Spotify" />
+      <Layout>Download App</Layout>
+    </>
+  )
+}
+
+export default DownloadPage
