1d2be7a01a81b0e48ce8ccd77eb82407504f9cd1
Alejandro Mejia <alejomejiacuartas@outlook.com>
2022-02-23 19:19:18 -0500

    remove unused lines of code for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,4 @@
 const path = require('path')
-const webpack = require('webpack')
 
 module.exports = {
   webpackFinal: async (config) => {
@@ -12,18 +11,6 @@ module.exports = {
       ...config.resolve.alias,
       '@assets': path.resolve(__dirname, '../public/assets'),
     }
-
-    config.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.__NEXT_IMAGE_OPTS': JSON.stringify({
-          deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-          imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-          domains: [],
-          path: '../public',
-          loader: 'default',
-        }),
-      })
-    )
 
     return config
   },
